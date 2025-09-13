2fb89104c3b88749c999525c96fd1f2fcc0e8df4
Your Name <suhasmashety@gmail.com>
2025-09-13 19:52:39 +0000

    added landingpage and dashboard

---
+++ src/Pages/LandingPage.tsx
@@ -0,0 +1,34 @@
+import { GridBackgroundDemo } from "../components/shadcomponents/GridBackground";
+import { Navigation, Hero, Stats, CallToAction, Features, Footer } from "../components/landingpagecomponents";
+
+interface LandingPageProps {
+  onGetStarted: () => void;
+}
+
+export function LandingPage({ onGetStarted }: LandingPageProps) {
+  return (
+    <div className="min-h-screen bg-black text-white overflow-hidden relative">
+      {/* Grid Background */}
+      <GridBackgroundDemo />
+      
+      {/* Navigation */}
+      <Navigation />
+
+      {/* Hero Section */}
+      <Hero onGetStarted={onGetStarted} />
+
+      {/* Stats Section */}
+      <Stats />
+
+      {/* Ready to Start Section */}
+      <CallToAction />
+
+      {/* Features Section */}
+      <Features />
+
+      {/* Footer */}
+      <Footer />
+      
+    </div>
+  );
+}
