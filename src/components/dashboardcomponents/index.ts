5446fafc452fe42de55cd5a0919e39a6702f8ed8
Your Name <suhasmashety@gmail.com>
2025-09-15 13:50:18 +0000

    added smart routing

--- src/components/dashboardcomponents/index.ts
+++ src/components/dashboardcomponents/index.ts
@@ -1,6 +1,8 @@
-export { Header } from './Header';
-export { VaultInfoCard } from './VaultInfoCard';
-export { UserBalancesCard } from './UserBalancesCard';
-export { ActionPanel } from './ActionPanel';
-export { DevelopmentUtils } from './DevelopmentUtils';
-export { ErrorDisplay } from './ErrorDisplay';
+export { Header } from "./Header";
+export { VaultInfoCard } from "./VaultInfoCard";
+export { UserBalancesCard } from "./UserBalancesCard";
+export { ActionPanel } from "./ActionPanel";
+export { DevelopmentUtils } from "./DevelopmentUtils";
+export { ErrorDisplay } from "./ErrorDisplay";
+export { SmartRouting } from "./SmartRouting";
+
