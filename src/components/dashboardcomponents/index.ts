0cafb456e2c993aa7522050d5fde0b296b1ae9f6
Your Name <suhasmashety@gmail.com>
2025-09-16 15:36:44 +0000

    added dashboard components

--- src/components/dashboardcomponents/index.ts
+++ src/components/dashboardcomponents/index.ts
@@ -1,8 +1,10 @@
 export { Header } from "./Header";
-export { VaultInfoCard } from "./VaultInfoCard";
-export { UserBalancesCard } from "./UserBalancesCard";
-export { ActionPanel } from "./ActionPanel";
 export { DevelopmentUtils } from "./DevelopmentUtils";
 export { ErrorDisplay } from "./ErrorDisplay";
 export { SmartRouting } from "./SmartRouting";
+export { PortfolioOverview } from "./PortfolioOverview";
+export { TabNavigation, useTabNavigation } from "./TabNavigation";
+export { AssetsDisplay } from "./AssetsDisplay";
+export { LiveRatesDisplay } from "./LiveRatesDisplay";
+export type { TabType } from "./TabNavigation";
 
