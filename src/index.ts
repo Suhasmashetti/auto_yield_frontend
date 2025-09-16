a1b04ce9225ffa93e7f5fe043c24d34e3b0121ad
Your Name <suhasmashety@gmail.com>
2025-09-16 18:18:41 +0000

    completed

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export { Header, VaultInfoCard, UserBalancesCard, ActionPanel, DevelopmentUtils, ErrorDisplay } from './components/dashboardcomponents';
+export { Header, DevelopmentUtils, ErrorDisplay } from './components/dashboardcomponents';
 export { LandingPage } from './Pages/LandingPage';
 export { Dashboard } from './Pages/Dashboard';
